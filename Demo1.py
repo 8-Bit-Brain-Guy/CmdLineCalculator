825b67ddee642caba87e16070ee8dcecb7c1ee3c
TimFromGermany <test252@web.de>
2022-08-20 23:24:06 +0200

    Demo1.py erstellt

---
+++ Demo1.py
@@ -0,0 +1,13 @@
+'''
+Demo1: Einfaches Demo einer Funktion mit einem Parameter und einem Rückgabenwert
+'''
+
+
+def Demofunktion(zahl):
+    print("Der übergebene Wert ist: %d" %zahl)
+    return zahl*2
+    
+    
+
+rueckgabewert = Demofunktion(100)
+print("Der Rückgabenwert ist %d" %rueckgabewert)
