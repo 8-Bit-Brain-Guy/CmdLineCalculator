603ae0a51f9b6e9419f81638bd2d952220199124
Steinpilz12 <maikbehrens62@yahoo.com>
2022-08-26 22:11:51 +0200

    Dateien addition etc hinzugefügt.

---
+++ multiplikation.py
@@ -0,0 +1,7 @@
+
+def multiplikation(zahl1,zahl2):
+    print("Der übergebene Wert ist: %d" %zahl1, "und %d" %zahl2)
+    return zahl1 * zahl2
+
+rueckgabewert = multiplikation(25,25)    
+print("Der Rueckgabewert ist %d" %rueckgabewert)    
