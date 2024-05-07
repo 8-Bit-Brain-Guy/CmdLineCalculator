c746a800da2bfd454b7ae444b5ee8d90de3a92c4
TimFromGermany <43537682+TimFromGermany@users.noreply.github.com>
2024-05-07 22:54:08 +0200

    Create Subtraktion.py

---
+++ Subtraktion.py
@@ -0,0 +1,6 @@
+def subtraktion(zahl1,zahl2):
+    print("Der übergebene Wert ist: %d" %zahl1, "und %d" %zahl2)
+    return zahl1 - zahl2
+
+rueckgabewert = subtraktion(25,25)    
+print("Der Rueckgabewert ist %d" %rueckgabewert)
