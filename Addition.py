e642c96e2832caef2c0c8c8117923705d49c884d
Tim Baumgarten <tbaumgarten@dspace.de>
2024-05-09 00:03:54 +0200

    1

--- Addition.py
+++
@@ -1,6 +0,0 @@
-def addition(zahl1, zahl2):
-    print("Der übergebene Wert ist: %d" %zahl1, "und %d" %zahl2)
-    return zahl1 + zahl2
-
-rueckgabewert = addition(25,25)    
-print("Der Rueckgabewert ist %d" %rueckgabewert)
---
+++ MathOperations.py
@@ -0,0 +1,30 @@
+def addition(zahl1, zahl2):
+    print("Der übergebene Wert ist: %d" %zahl1, "und %d" %zahl2)
+    return zahl1 + zahl2
+
+rueckgabewert = addition(25,25)    
+print("Der Rueckgabewert ist %d" %rueckgabewert)
+
+
+def subtraktion(zahl1,zahl2):
+    print("Der übergebene Wert ist: %d" %zahl1, "und %d" %zahl2)
+    return zahl1 - zahl2
+
+rueckgabewert = subtraktion(25,25)    
+print("Der Rueckgabewert ist %d" %rueckgabewert)
+
+
+def division(zahl1, zahl2):
+    print("Der übergebene Wert ist: %d" %zahl1, "und %d" %zahl2)
+    return zahl1 / zahl2
+
+rueckgabewert = division(25,25)    
+print("Der Rueckgabewert ist %d" %rueckgabewert)   
+
+
+def multiplikation(zahl1,zahl2):
+    print("Der übergebene Wert ist: %d" %zahl1, "und %d" %zahl2)
+    return zahl1 * zahl2
+
+rueckgabewert = multiplikation(25,25)    
+print("Der Rueckgabewert ist %d" %rueckgabewert)   
